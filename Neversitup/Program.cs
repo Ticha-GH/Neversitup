6adfc00f3ba92a088c8182fddfd96c45ea42bf49
Ticha <ticha.u.toey@gmail.com>
2024-08-09 23:15:16 +0700

    [add] test c#

---
+++ Neversitup/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+public class Program
+{
+
+    public string[] Shuffle(string word)
+    {
+        if (word.Length == 1)
+        {
+            return new string[] { word };
+        }
+        else
+        {
+            List<string> words = new List<string>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                char parent = word[i];
+                string child = $"{word.Substring(0, i)}{word.Substring(i + 1)}";
+                string[] childs = Shuffle(child);
+                for (int j = 0; j < childs.Length; j++)
+                {
+                    string newWord = $"{parent}{childs[j]}";
+                    bool duplicate = words.Count > 0 && words.Any(x => words.Contains(newWord));
+                    if (!duplicate)
+                    {
+                        words.Add(newWord);
+                    }
+                }
+            }
+            return words.ToArray();
+        }
+    }
+
+    public string FindOdd(int[] numbers)
+    {
+        List<CountModel> counts = new List<CountModel>();
+
+        foreach (int number in numbers)
+        {
+            CountModel model = counts.Where(x => x.Number == number).FirstOrDefault();
+            if (model is not null)
+            {
+                model.Count++;
+            }
+            else
+            {
+                counts.Add(new CountModel() { Number = number, Count = 1 });
+            }
+
+        }
+
+        counts = counts.Where(x => x.Count % 2 != 0).ToList();
+        if (counts.Count != 1)
+        {
+            return "";
+        }
+
+        CountModel result = counts.FirstOrDefault();
+        var str = $"{result.Number}, because ot occurs {result.Count} times (which is odd)";
+        return str;
+    }
+
+    public int SmileyFaces(string[] faces)
+    {
+        List<string> smileFaces = new List<string> { };
+        char[] eyes = new char[] { ':', ';' };
+        char[] noses = new char[] { '-', '~' };
+        char[] smailes = new char[] { ')', 'D' };
+
+        foreach (var face in faces)
+        {
+            if (!string.IsNullOrEmpty(face))
+            {
+                int i = 0;
+                if (eyes.Any(x => x == face[i]))
+                {
+                    i++;
+
+                    if (noses.Any(x => x == face[i]))
+                    {
+                        i++;
+                    }
+
+                    if (smailes.Any(x => x == face[i]))
+                    {
+                        smileFaces.Add(face);
+                    }
+                }
+            }
+        }
+
+        return smileFaces.Count;
+    }
+
+    public class CountModel
+    {
+        public int Number { get; set; }
+        public int Count { get; set; }
+    }
+
+    public static void Main()
+    {
+    }
+
+}
+
---
+++ NeversitupTests/ProgramTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        private Program _service;
+
+
+        [TestMethod()]
+        [DataRow("a", new string[] { "a" })]
+        [DataRow("ab", new string[] { "ab", "ba" })]
+        [DataRow("abc", new string[] { "abc", "acb", "bac", "bca", "cab", "cba" })]
+        [DataRow("aabb", new string[] { "aabb", "abab", "abba", "baab", "baba", "bbaa" })]
+        public void ShuffleTest(string word, string[] expected)
+        {
+            //prepare
+            _service = new Program();
+
+            //act
+            var actual = _service.Shuffle(word);
+
+            //asset
+            Assert.IsNotNull(actual);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        // $"{result.Number}, because ot occurs {result.Count} times (which is odd)
+        [TestMethod()]
+        [DataRow(new int[] { 7 }, "7, because ot occurs 1 times (which is odd)")]
+        [DataRow(new int[] { 0 }, "0, because ot occurs 1 times (which is odd)")]
+        [DataRow(new int[] { 1, 1, 2 }, "2, because ot occurs 1 times (which is odd)")]
+        [DataRow(new int[] { 0, 1, 0, 1, 0 }, "0, because ot occurs 3 times (which is odd)")]
+        [DataRow(new int[] { 1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1 }, "4, because ot occurs 1 times (which is odd)")]
+        public void FindOddTest(int[] numbers, string expected)
+        {
+            //prepare
+            _service = new Program();
+
+            //act
+            var actual = _service.FindOdd(numbers);
+
+            //asset
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DataRow(new string[] { "" }, 0)]
+        [DataRow(new string[] { "0" }, 0)]
+        [DataRow(new string[] { }, 0)]
+        [DataRow(new string[] { ":)", ";(", ";}", ":-D" }, 2)]
+        [DataRow(new string[] { ";D", ":-(", ":-)", ";~)" }, 3)]
+        [DataRow(new string[] { ";]", ":[", ":$", ";-D" }, 1)]
+        public void SmileyFacesTest(string[] faces, int expected)
+        {
+            //prepare
+            _service = new Program();
+
+            //act
+            var actual = _service.SmileyFaces(faces);
+
+            //asset
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
